aa9286538687a0e3d3f73920547a80ba10051c7e
Gillian0023 <jilin0023@gmail.com>
2016-12-09 14:52:43 +0100

    inital alarm

---
+++ dist/server/alarm/alarm.js
@@ -0,0 +1,7 @@
+"use strict";
+const express_1 = require('express');
+const AlarmRouter = express_1.Router();
+exports.AlarmRouter = AlarmRouter;
+AlarmRouter.post('/',AlarmRouter);
+AlarmRouter.get('/:id',AlarmRouter);
+//# sourceMappingURL=/Users/jilin/Programming/contextualcollabnodejs/dist/server/alarm/alarm.js.map
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,24 +6,17 @@ import { Observable } from 'rxjs';
 import 'rxjs/add/operator/catch';
 import 'rxjs/add/operator/map';
 
-import { ChatService } from './chatservice';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './login/login.component.html',
   styleUrls: ['./app.component.css'],
-  providers: [ChatService]
 })
 export class AppComponent  {
   title = 'Contextual Collaboration';
 
   observable$: Observable<{}>;
-
-  constructor(http: Http, private chatService:ChatService) {
-    this.observable$ = http
-      .get('/api/public/simple')
-      .map((response: Response) => response.json());
-  }
 
   ngOnInit() {
   }
